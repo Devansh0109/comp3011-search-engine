548869dbf1a55efbfd5e545f6ee376b1fc742dcb
Devansh0109 <157219765+Devansh0109@users.noreply.github.com>
2026-05-04 12:30:59 +0100

    Basic inverted index storing word occurrences

---
+++ src/indexer.py
@@ -0,0 +1,14 @@
+def build_index(pages):
+    index = {}
+
+    for url, text in pages.items():
+        words = text.lower().split()
+
+        for word in words:
+            if word not in index:
+                index[word] = []
+
+            if url not in index[word]:
+                index[word].append(url)
+
+    return index
